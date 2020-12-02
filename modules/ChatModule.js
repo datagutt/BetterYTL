562c8b9b4fa15cbdfb10aa78f5a19f8945bc2f3e
datagutt <datagutt@lekanger.no>
2020-12-02 01:41:48 +0100

    Add timestamp and channel links

--- modules/ChatModule.js
+++ modules/ChatModule.js
@@ -24,7 +24,8 @@ const timeConversion = function (s) {
     if(s.endsWith('AM') && d.getHours() === 12) d.setHours(d.getHours() - 12);
 
     let result = (d.getHours() < 10 ? '0' + d.getHours() : d.getHours()) + ':' +
-        (d.getMinutes() < 10 ? '0' + d.getMinutes() : d.getMinutes());
+        (d.getMinutes() < 10 ? '0' + d.getMinutes() : d.getMinutes()) + ':' +     
+        (d.getSeconds() <10 ? "0"+ d.getSeconds(): d.getSeconds());
 
     return result;
 }
@@ -46,7 +47,13 @@ var placeCaretAtEnd = function(el){
         textRange.select();
     }
 }
+function wrap(el, wrapper) {
+    el.parentNode.insertBefore(wrapper, el);
+    wrapper.appendChild(el);
+}
 
+const template = document.createElement('span');
+template.innerHTML = ` <a target="_blank" style='color: gray; text-decoration: none'>(U)</a>`;
 export class Message {
     constructor(node){
         this.node = node;
@@ -55,7 +62,9 @@ export class Message {
         this.parsedText = ''; // This should be fine since you can't edit/change messages
         // Allow clicking author name to mention
         this.destroyed = false;
+        this.loopCount = 0;
         if(this.node){
+            this.addChannelLink();
             this.clickToMention();
             this.parseText();
             this.node.setAttribute('message-id', this.id);
@@ -64,12 +73,36 @@ export class Message {
         }
     }
     get textNode() {
-        const node = this.node.querySelector('#content #message');
+        const node = this.node.$.message;
         return {
             node,
             text: node.innerText,
             html: node.innerHTML
         };
+    }
+    addChannelLink(){
+        var el = this.node;
+        if(el.getAttribute('link-applied') !== 'true'){
+            const span = template.cloneNode(true);
+            const a = span.querySelector('a');
+
+            // Did we get notified too fast? Hopefully if this is empty, we'll get things fixed up at dataChanged_?
+            if(el.data === undefined){
+                log('data is undefined, doing nothing for now');
+            }else{
+                a.href = `https://www.youtube.com/channel/${el.data.authorExternalChannelId}`;
+            }
+
+            el.userLink = a;
+            wrap(el.querySelector('#author-name'), span);
+            el.setAttribute('link-applied', '');
+
+            const originalDataChanged = el.dataChanged_.bind(el);
+            el.dataChanged_ = (niw, old) => {
+                originalDataChanged(niw, old);
+                el.userLink.href = `https://www.youtube.com/channel/${niw.authorExternalChannelId}`;
+            };
+        }
     }
     clickToMention(){
         this.node.querySelector('#author-name').addEventListener('click', function () {
@@ -83,41 +116,63 @@ export class Message {
     parseText(){
          // Get the actual text of the message
          var textNode = this.textNode;
-         console.log('textNode', textNode);
  
-         var timestamp = this.node.querySelector('#timestamp');
+         // Fix timestamp (AM/PM to european format)
+         var timestamp = this.node.$.timestamp;
          if(timestamp) timestamp.innerText = timeConversion(timestamp.innerText);
-         // Get the auhtor of the message
+
+         // Fix tooltip on badges
+         var badges = this.node.querySelectorAll('yt-live-chat-author-badge-renderer');
+         badges.forEach(badge => {
+             badge.title = badge.getAttribute('shared-tooltip-text');
+         });
+         // Fix tooltip on emotes
+         var emotes = this.node.querySelectorAll('img.emoji');
+         if(emotes && emotes.length !== 0) console.log('emotes', emotes);
+         emotes.forEach(emote => {
+             console('yo emote', emote.title, emote.attributes);
+             emote.title = emote.getAttribute('shared-tooltip-text');
+         });
+         // Get the author of the message
          var authorName = this.node.querySelector('#author-name');
          var authorColor = stringToColour(authorName.textContent.trim());
          var authorType = this.node.getAttribute('author-type');
+         // If not owner, change author color
          if(authorType !== 'owner'){ 
              authorName.style.color = authorColor;
-             console.log(authorName, authorName.textContent.trim(), authorColor);
          }
-     
          var innerHtml = ` ${textNode.html} `;
  
+         // Add emotes if words are noticed
          var wordsArray = innerHtml.split(' ');
          wordsArray.forEach((word, i) => {
              var emote = emotes[word];
              if(emote !== undefined){
-                 var imgHtml = `<img class='ytl-emote emoji style-scope yt-live-chat-text-message-renderer' src='${emote.url}' alt=':${word}:' data-emoji-id='${word}' shared-tooltip-text=':${word}:' /> `;    
+                 var imgHtml = `<img class='ytl-emote emoji style-scope yt-live-chat-text-message-renderer' src='${emote.url}' title='${word}' alt='${word}' data-emoji-id='${word}' shared-tooltip-text='${word}' /> `;    
                  innerHtml = innerHtml.replace(' ' + word + ' ', ' ' + imgHtml + ' ');
              }
          });
          innerHtml = innerHtml.substring(1, innerHtml.length - 1);
-         this.parsedText = innerHtml.replace('﻿', '').trim();
+         this.parsedText = innerHtml.trim();
     }
     watch(){
+        
+        
         this.observer = new MutationObserver(mutations => {
+            this.loopCount++;
+            if(this.loopCount >= 5){
+                console.log('we looped a lot for this msg', this.loopCount, this);
+                return;
+            }
             if(this.destroyed) return;
             if(
                 document.body.contains(this.node) &&
-                this.textNode.html !== this.parsedText
-            ) {
+                this.textNode.html.trim() !== this.parsedText.trim()
+                && this.id === this.node.id
+                && this.id === this.node.getAttribute('mesasge-id')
+            ){
                 this.textNode.node.innerHTML = this.parsedText;
-                this.parsedText = this.textNode.node.innerHTML;
+                //this.parsedText = this.textNode.node.innerHTML;
             }
         });
         this.observer.observe(this.node, {
@@ -139,8 +194,8 @@ export default class ChatModule {
     messages = new Map();
     observer = null;
     async start(){
+        const messages = document.querySelector('#items.style-scope.yt-live-chat-item-list-renderer');
         const chatApp = document.querySelector('yt-live-chat-app');
-        const messages = document.querySelector('#items.yt-live-chat-item-list-renderer');
         let chatMagicDone =
             chatApp &&
             chatApp.getAttribute('data-betterytl') === 'true';
@@ -150,12 +205,14 @@ export default class ChatModule {
             chatApp.setAttribute('data-betterytl', true);
             chatMagicDone = true;
 
+            // Add seconds to message timestamp
+            this.addSecondsToTimestamp();
             // Force live
             this.forceLive();
 
             // Add streamlabs button
             this.addDonationButton();
-
+            
             // Observe new chat messages
             this.observer = new MutationObserver(this.mutator);
 
@@ -167,7 +224,7 @@ export default class ChatModule {
             });
 
             // Preloaded messages
-            Array.from(messages.querySelectorAll('yt-live-chat-text-message-renderer')).forEach(node => {
+            document.querySelectorAll('#items.style-scope.yt-live-chat-item-list-renderer yt-live-chat-text-message-renderer').forEach(node => {
                 if(node) this.onChatMessage(node);
             });
         }else{
@@ -176,6 +233,13 @@ export default class ChatModule {
     }
     addDonationButton(){
        var buttons = document.querySelector('#input-panel #container > #buttons #picker-buttons');
+    }
+    addSecondsToTimestamp(){
+        let messageRenderer = document.querySelector('yt-live-chat-text-message-renderer');
+        let rendererPrototype = Object.getPrototypeOf(messageRenderer);
+
+        rendererPrototype.TIME_FORMATTER.patternParts_ = [];
+        rendererPrototype.TIME_FORMATTER.applyPattern_('h:mm:ss a');      
     }
     forceLive(){
         var liveBtn = document.querySelector('#view-selector a:nth-child(2) paper-item')
@@ -189,6 +253,7 @@ export default class ChatModule {
     }
     onChatMessageRemoved = (entry) => {
         const messageId = entry.getAttribute('message-id');
+        if(!messageId) return;
         const message = this.messages.get(messageId);
         if(message !== undefined){
             message.destroyed = true;
@@ -207,13 +272,12 @@ export default class ChatModule {
                     let node = addedNodes[i];
                     if(node instanceof HTMLElement){
                         const tags = [
-                            'yt-live-chat-banner-renderer',
                             'yt-live-chat-text-message-renderer',
                             'yt-live-chat-paid-message-renderer',
                             'yt-live-chat-legacy-paid-message-renderer',
                         ];
                         if(tags.includes(node.tagName.toLowerCase())){
-                            this.onChatMessage(node, true);
+                            this.onChatMessage(node);
                         }
                     }
                 }
@@ -224,14 +288,13 @@ export default class ChatModule {
                     const node = removedNodes[i];
                     if(node instanceof HTMLElement){
                         const tags = [
-                            'yt-live-chat-banner-renderer',
                             'yt-live-chat-text-message-renderer',
                             'yt-live-chat-paid-message-renderer',
                             'yt-live-chat-legacy-paid-message-renderer',
                         ];
                         if(tags.includes(node.tagName.toLowerCase())){
                             console.log('removed message', node.id);
-                             this.onChatMessageRemoved(node);
+                            this.onChatMessageRemoved(node);
                         }
                     }
                   }
